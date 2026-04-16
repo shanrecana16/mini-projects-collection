00b30f9e8015c34bfac31c35c19e5388b6b35534
Shanmickle Recaña <shanrecana16@gmail.com>
2026-04-16 17:59:24 +0800

    Add initial React to-do list project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import './reset.css'
+
+function App() {
+  return (
+   <header class="header">
+    <div class="header_content">
+      <h1 class="header_title">To do list App</h1>
+      <div class="header_nav-panel">
+        <div class="header_nav-panel">
+          <input type="text" class="field_input" placeholder='Search note...'/>
+        </div>
+      </div>
+    </div>
+   </header>
+  )
+}
+
+export default App
